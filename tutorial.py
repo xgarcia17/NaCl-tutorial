87502a60545cc8da1c5e722d4333be1fc5ecec25
Xavier <xmangarcia17@gmail.com>
2024-06-26 16:07:58 -0700

    get VASP to work with code after configuring calc = Vasp(. . .)

--- tutorial.py
+++ tutorial.py
@@ -5,49 +5,54 @@ HOW:    Take the code from PySAGES water example and refit it to work for NaCl u
 
 import numpy as np
 from ase import Atoms, units
-from ase.calculators.tip3p import TIP3P, angleHOH, rOH
 from ase.constraints import FixBondLengths
 from ase.io.trajectory import Trajectory
 from ase.md import Langevin
+from ase.calculators.vasp import Vasp
 
+rNaCl = 2.8    # distasnce between Na and Cl atoms in Angstroms
+angleNaCl = 180
 
-def generate_simulation(tag="tip3p", write_output=True):
-
-    # angleHOH and rOH are imported variables, so names must remain as the is
-
-    x = angleHOH * np.pi / 180 / 2
+def generate_simulation(tag="custom_NaCl", write_output=True):
     pos = [
-        [0, 0, 0],  # rOH is the distance between oxygen and hydrogen atoms in water
-        [0, rOH * np.cos(x), rOH * np.sin(x)],
-        # [0, rOH * np.cos(x), -rOH * np.sin(x)],
+        [0, 0, 0],
+        [0, 0, rNaCl]
     ]
-    atoms = Atoms("Na+Cl", positions=pos)
 
+    atoms = Atoms("NaCl", positions=pos)
 
-    # atoms = Atoms("OH2", positions=pos)
-
-    vol = ((18.01528 / 6.022140857e23) / (0.9982 / 1e24)) ** (1 / 3.0)
+    vol = 20
     atoms.set_cell((vol, vol, vol))
     atoms.center()
 
-    atoms = atoms.repeat((3, 3, 3))
     atoms.set_pbc(True)
 
-    atoms.constraints = FixBondLengths(
-        # [(3 * i + j, 3 * i + (j + 1) % 3) for i in range(3**3) for j in [0, 1, 2]]
-        [(2 * i + j, 2 * i + (j + 1) % 2) for i in range(3**3) for j in [0, 1]]
-    )
-
     T = 300 * units.kB
-    atoms.calc = TIP3P(rc=4.5)
+    calc = Vasp(
+        xc='PBE',
+        encut=300,
+        kpts=(1,1,1),
+        ibrion=2,
+        nsw=50,
+        potim=1.0,
+        ismear=0,
+        sigma=0.05,
+        lreal='Auto'
+    )
+    # atoms.calc = calc.initialize(atoms)
+    calc.initialize(atoms)
+    atoms.set_calculator(calc)
+    calc.write_input(atoms)
     logfile = tag + ".log" if write_output else None
-    md = Langevin(atoms, 1 * units.fs, temperature_K=T, friction=0.01, logfile=logfile)
+    dyn = Langevin(atoms, 1 * units.fs, temperature_K=T, friction=0.01, logfile=logfile)
 
     if write_output:
         traj = Trajectory(tag + ".traj", "w", atoms)
-        md.attach(traj.write, interval=1)
+        dyn.attach(traj.write, interval=1)
+    
+    dyn.run(5)
 
-    return md
+    return dyn
 
 
 
